d3fb8e11eb25ee8a7d5e77293467c71c584e267b
Rahil <44476072+Rahilkaxi@users.noreply.github.com>
2020-04-27 04:35:08 +0530

    Add files via upload

---
+++ url.py
@@ -0,0 +1,12 @@
+#https://drv.tw/~[username]/[drive]/[path]
+
+url ="https://drv.tw/~"
+
+email = input("Type Your Email > ")   
+
+drive ="gd"
+
+path = input("Type Your File Path > ")
+
+print(url + email + drive + path)
+
