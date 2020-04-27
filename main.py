870bd8371a722c94406de37b8cc0517622829078
Rahil <44476072+Rahilkaxi@users.noreply.github.com>
2020-04-27 13:16:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import os
  
 
 hello = input("[?] Do you want to update? (y/n): ")
-        print("================================")
-        if hello == "y":
-            print("========================================================")
-            print("[+] Please wait...")
-            print("========================================================")
-            os.system("pkg install git")
-            os.system("cd /data/data/com.termux/files/home && rm -rf drivetoWeb")
-            os.system("/data/data/com.termux/files/home && https://github.com/Rahilkaxi/DrivetoWeb.git")
-            os.system("cd DrivetoWeb")
-            os.system("python run.py")
+print("================================")
+if hello == "y":
+    print("========================================================")
+    print("[+] Please wait...")
+    print("========================================================")
+    os.system("pkg install git")
+    os.system("cd /data/data/com.termux/files/home && rm -rf drivetoWeb")
+    os.system("/data/data/com.termux/files/home && https://github.com/Rahilkaxi/DrivetoWeb.git")
+    os.system("cd DrivetoWeb")
+    os.system("python run.py")
