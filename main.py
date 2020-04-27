63a5ee2d6b1463f208611f810553271c114a7278
Rahil <44476072+Rahilkaxi@users.noreply.github.com>
2020-04-27 12:19:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#importing os module  
+import os 
+ 
+
+
+#Command to execute 
+#Using Windows OS command 
+  cmd = 'apt install git' 
+ 
+#Using os.system() method 
+  os.system (cmd) 
+ os.system ('mkdir' + 'test 2') 
+#os.system()
+#os.system()
+#os.system()
