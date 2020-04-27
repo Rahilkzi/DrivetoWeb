5a4a37cca5a0f41b4b085e0cd75572daa2b704f6
Rahil <44476072+Rahilkaxi@users.noreply.github.com>
2020-04-27 11:58:53 +0530

    Update run.py

--- run.py
+++ run.py
@@ -14,7 +14,3 @@ get = (url + email + "/" + drive + "/" + path)
 
 print ("♨️this is your URL▶️ " + get)
 
-
-inp=input("\033[1;33m Do you want to install Tool-X [Y/n]> \033[00m")
-if inp=="y" or inp=="Y":
-    print("apt install git")
