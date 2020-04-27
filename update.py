c37a572b2cb3ae24a385527c002742718adc6c25
Rahil <44476072+Rahilkaxi@users.noreply.github.com>
2020-04-27 15:16:50 +0530

    Update update.py

--- update.py
+++ update.py
@@ -1,6 +1,35 @@
-#importing os module  
-import os 
- 
+import os
+
+
+red= '\033[91m'
+orange= '\33[38;5;208m'
+green= '\033[92m'
+cyan= '\033[36m'
+bold= '\033[1m'
+end= '\033[0m'
+
+
+os.system('clear')
+p1='''{5}
+           ▀▄   ▄▀
+          ▄█▀███▀█▄
+         █▀███████▀█
+         █─█▀▀▀▀▀█─█
+            ▀▀─▀▀
+ *****    Mob-Droid   *****
+ '''.format(orange, green, bold, end, cyan, red)
+print(p1) 
+
+
+p2='''{5}       
+{2}Follow me :{3}
+{1}•{3} GitHub : {4}https://github.com/rahilkaxi{3}
+{1}•{3} YouTube: {4}soon{3}
+{1}•{3} Website: {4}soon{3}
+'''.format(orange, green, bold, end, cyan, red)
+
+print(p2)
+
 
 hello = input("[?] Do you want to update? (y/n): ")
 print("================================")
